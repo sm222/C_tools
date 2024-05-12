3f62e576a2f138074f2d78ab388ed3065d78875c
sm222 <soleil.maraska@gmail.com>
2024-05-12 18:44:04 -0400

    clean code

--- Ct_lib/inc/Ct_get_next_line.h
+++ Ct_lib/inc/Ct_get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Ct_get_next_line.h                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: oboucher <oboucher@student.42.fr>          +#+  +:+       +#+        */
+/*   By: anboisve <anboisve@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 13:32:00 by anboisve          #+#    #+#             */
-/*   Updated: 2023/08/13 15:31:55 by oboucher         ###   ########.fr       */
+/*   Updated: 2024/05/12 18:43:41 by anboisve         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,15 @@
 #  define BUFFER_SIZE 100
 # endif
 
-typedef struct s_Ct_info
-{
-	char	*tmp;
-	char	*tmp2;
-	int		rv;
-	size_t	cut;
-}	t_Ct_info;
+typedef struct s_Ct_info {
+  char*   tmp;
+  char*   tmp2;
+  int     rv;
+  size_t  cut;
+}  t_Ct_info;
 
-void	*Ct_calloc(size_t size, size_t count);
-char	*Ct_get_next_line(int fd);
-void	Ct_bzero(void *p, size_t size);
+void*  Ct_calloc(size_t size, size_t count);
+char*  Ct_get_next_line(int fd);
+void   Ct_bzero(void* p, size_t size);
 
 #endif // GET_NEXT_LINE_H
